16b9d1f71d2c3c4d3bff8746ee6026ffdee736d3
Loic Blot <loic.blot@unix-experience.fr>
2024-03-26 14:49:38 +0100

    feat: prepare basic kamaji object management

--- pkg/malygos/clustermanager/kamaji_manager.go
+++ pkg/malygos/clustermanager/kamaji_manager.go
@@ -1,8 +1,20 @@
 package clustermanager
 
 import (
+	"context"
+	"fmt"
+
+	kamaji "github.com/clastix/kamaji/api/v1alpha1"
 	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/utils/ptr"
+)
+
+const (
+	regionalClusterLabel = "malygos.local/region"
+	clusterIDLabel       = "malygos.local/cluster-id"
 )
 
 type KamajiClusterManager struct {
@@ -18,10 +30,60 @@ func NewKamajiClusterManager(logger logr.Logger, client *kubernetes.Clientset) *
 }
 
 func (m *KamajiClusterManager) Create(cluster *Cluster) (*Cluster, error) {
+	kamajiCluster := &kamaji.TenantControlPlane{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: cluster.ID,
+			Labels: map[string]string{
+				regionalClusterLabel: cluster.Region,
+			},
+		},
+		Spec: kamaji.TenantControlPlaneSpec{
+			DataStore: "default",
+			ControlPlane: kamaji.ControlPlane{
+				Deployment: kamaji.DeploymentSpec{
+					Replicas: ptr.To(int32(3)),
+					Resources: &kamaji.ControlPlaneComponentsResources{
+						APIServer:         &v1.ResourceRequirements{},
+						ControllerManager: &v1.ResourceRequirements{},
+						Scheduler:         &v1.ResourceRequirements{},
+						Kine:              &v1.ResourceRequirements{},
+					},
+				},
+				Service: kamaji.ServiceSpec{
+					AdditionalMetadata: kamaji.AdditionalMetadata{
+						Labels: map[string]string{
+							clusterIDLabel:       cluster.ID,
+							regionalClusterLabel: cluster.Region,
+						},
+					},
+				},
+			},
+			Kubernetes: kamaji.KubernetesSpec{
+				Version: "1.28.1",
+				Kubelet: kamaji.KubeletSpec{
+					CGroupFS: "systemd",
+				},
+			},
+			NetworkProfile: kamaji.NetworkProfileSpec{},
+			Addons: kamaji.AddonsSpec{
+				CoreDNS: &kamaji.AddonSpec{},
+			},
+		},
+	}
+
+	err := m.client.RESTClient().Post().Resource("tenantcontrolplanes").Body(kamajiCluster).Do(context.TODO()).Into(kamajiCluster)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kamaji cluster: %v", err)
+	}
+
 	return cluster, nil
 }
 
 func (m *KamajiClusterManager) Delete(id string) error {
+	err := m.client.RESTClient().Delete().Resource("tenantcontrolplanes").Name(id).Do(context.TODO()).Into(&kamaji.TenantControlPlane{})
+	if err != nil {
+		return fmt.Errorf("failed to delete kamaji cluster: %v", err)
+	}
 	return nil
 }
 
